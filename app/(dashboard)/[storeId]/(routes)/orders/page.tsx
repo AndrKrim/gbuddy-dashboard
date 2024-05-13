1d4f40d51deb00387032a7f81678950feb87a10d
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-05-13 16:19:21 +0700

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -12,38 +12,11 @@ const OrdersPage = async ({
 }: {
   params: { storeId: string }
 }) => {
-  const orders = await prismadb.order.findMany({
-    where: {
-      storeId: params.storeId
-    },
-    include: {
-      orderItems: {
-        include: {
-          product: true
-        }
-      }
-    },
-    orderBy: {
-      createdAt: 'desc'
-    }
-  });
-
-  const formattedOrders: OrderColumn[] = orders.map((item) => ({
-    id: item.id,
-    phone: item.phone,
-    address: item.address,
-    products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
-    totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
-      return total + Number(item.product.price)
-    }, 0)),
-    isPaid: item.isPaid,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <OrderClient data={formattedOrders} />
+        <h1>Ini Halaman Order History</h1>
       </div>
     </div>
   );
