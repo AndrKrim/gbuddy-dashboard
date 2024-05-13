82be302cc07c4da49e5f4119a17248b9a9f5620a
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-05-13 16:22:43 +0700

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -9,33 +9,11 @@ const ProductsPage = async ({
 }: {
   params: { storeId: string }
 }) => {
-  const products = await prismadb.product.findMany({
-    where: {
-      storeId: params.storeId
-    },
-    include: {
-      category: true,
-    },
-    orderBy: {
-      createdAt: 'desc'
-    }
-  });
-
-  const formattedProducts: ProductColumn[] = products.map((item) => ({
-    id: item.id,
-    name: item.name,
-    description: item.description,
-    isFeatured: item.isFeatured,
-    isArchived: item.isArchived,
-    price: formatter.format(item.price.toNumber()),
-    category: item.category.name,
-    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
-  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductsClient data={formattedProducts} />
+        <h1>Ini Halaman Produk</h1>
       </div>
     </div>
   );
