cb04458bcc23dc49b4ba6318da1c81fa927f7482
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-05-20 10:00:16 +0700

    Update dashboard page.tsx

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -16,11 +16,56 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+    const stockCount = await getStockCount(params.storeId);
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <Heading title="Ini Dashboard" description="Halaman Dashboard" />
+                <Heading title="Dashboard" description="Ini Halaman Dashboard" />
+
+                <Separator />
+
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Total Revenue
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                $ 50.00
+                            </div>
+                        </CardContent>
+                    </Card>
+                        
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                            <CreditCard className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                                
+                        <CardContent>
+                            <div className="text-2xl font-bold">+17</div>
+                        </CardContent>
+                    </Card>
+                        
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                                Products In Stock
+                            </CardTitle>
+                                
+                            <Package className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                            
+                        <CardContent>
+                            <div className="text-2xl font-bold">{stockCount}</div>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     );
