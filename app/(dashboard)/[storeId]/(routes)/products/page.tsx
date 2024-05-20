7973eef4061945a09abadfb8ce0829684f8b0660
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-05-20 15:01:41 +0700

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -35,7 +35,6 @@ const ProductsPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <h1>Ini Halaman Product</h1>
         <ProductsClient data={formattedProducts} />
       </div>
     </div>
