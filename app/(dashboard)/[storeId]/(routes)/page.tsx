6509442d24cb90f8dda72b71467364a5aebbed5d
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-05-21 14:57:20 +0700

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -16,6 +16,9 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+    const totalRevenue = await getTotalRevenue(params.storeId);
+    const graphRevenue = await getGraphRevenue(params.storeId);
+    const salesCount = await getSalesCount(params.storeId);
     const stockCount = await getStockCount(params.storeId);
 
     return (
