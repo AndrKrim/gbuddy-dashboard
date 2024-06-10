3fa74ec34eec62ad3e6d29046389480cccbdd582
AndrKrim <120653064+AndrKrim@users.noreply.github.com>
2024-06-10 16:15:00 +0700

    Update page.tsx

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -6,46 +6,6 @@ import { formatter } from "@/lib/utils";
 import { OrderColumn } from "./components/columns"
 import { OrderClient } from "./components/client";
 
-
-const dummyOrders = [
-  {
-    id: "1",
-    phone: "123-456-7890",
-    address: "123 Main St",
-    orderItems: [
-      {
-        product: { name: "Product 1", price: 12900 },
-      },
-    ],
-    isPaid: true,
-    createdAt: new Date("2023-01-01T10:00:00Z"),
-  },
-  {
-    id: "2",
-    phone: "987-654-3210",
-    address: "456 Elm St",
-    orderItems: [
-      {
-        product: { name: "Product 3", price: 190990 },
-      },
-    ],
-    isPaid: false,
-    createdAt: new Date("2023-02-15T14:30:00Z"),
-  },
-  {
-    id: "3",
-    phone: "555-555-5555",
-    address: "789 Oak St",
-    orderItems: [
-      {
-        product: { name: "Product 4", price: 390990 },
-      },
-    ],
-    isPaid: true,
-    createdAt: new Date("2023-03-20T08:45:00Z"),
-  },
-];
-
 const OrdersPage = async ({
   params
 }: {
@@ -67,7 +27,7 @@ const OrdersPage = async ({
     }
   });
 
-  const formattedOrders: OrderColumn[] = dummyOrders.map((item) => ({
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
     phone: item.phone,
     address: item.address,
